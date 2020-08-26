dc7031c762234b7a0a5b70208b3d6e94fa34ad66
coding-geek-shubham <sj1079268@gmail.com>
2020-08-27 00:52:25 +0530

    Adding workbook, making sheet variable and printing a cell value

--- Spreadsheet_Processing.py
+++ Spreadsheet_Processing.py
@@ -1,2 +1,8 @@
-import openpyxl as xl
 # importing openpyxl python lib as elios xl
+import openpyxl as xl
+# Opening xl workbook
+wb = xl.load_workbook('transaction.xlsx')
+sheet = wb['Sheet1']
+cell = sheet['a1']
+cell = sheet.cell(1,1)  # Returns the exact same cell
+print(cell.value)
